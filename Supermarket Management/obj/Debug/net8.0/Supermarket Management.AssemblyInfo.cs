b279eadda10125ded24ce2b81fedb1a09a9826f3
YoungGunner <youngpac.gunner14@gmail.com>
2024-04-17 11:02:25 +0545

    Added Edit Button

--- Supermarket Management/obj/Debug/net8.0/Supermarket Management.AssemblyInfo.cs
+++ Supermarket Management/obj/Debug/net8.0/Supermarket Management.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Supermarket Management")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0a241f3bc199dc16fb7b6162a47537660132fc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Supermarket Management")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Supermarket Management")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
